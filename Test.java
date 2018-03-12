80620adf90489d54f9d719b9f1385b78d59304b1
Administrator <1206369517@qq.com>
2018-03-12 14:13:18 +0800

    Commit Message

---
+++ Test.java
@@ -0,0 +1,10 @@
+package com.bdqn.test;
+
+/**
+ * Created by Administrator on 2018/3/12.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("000");
+    }
+}
