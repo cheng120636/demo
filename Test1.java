67f64fff846d70afdf087fc1af913d36263e8278
Administrator <1206369517@qq.com>
2018-03-12 14:31:17 +0800

    新加

---
+++ Test1.java
@@ -0,0 +1,15 @@
+package com.bdqn.test;
+
+/**
+ * Created by Administrator on 2018/3/12.
+ */
+public class Test1 {
+    public static void main(String[] args) {
+
+
+    int a = 4;
+    int b = a+2;
+    int c = a+b;
+        System.out.println(c);
+    }
+}
